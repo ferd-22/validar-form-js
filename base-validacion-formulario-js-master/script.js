42975f366fc1124933313a7cd6e1368fec9a8347
FerCastellini <118540112+ferd-22@users.noreply.github.com>
2024-02-21 00:22:36 -0300

    Add files via upload

---
+++ base-validacion-formulario-js-master/script.js
@@ -0,0 +1,80 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDOzfDdd66a91gvyUhiass73AR6wX8Gs-M",
+  authDomain: "datos-del-form.firebaseapp.com",
+  projectId: "datos-del-form",
+  storageBucket: "datos-del-form.appspot.com",
+  messagingSenderId: "961839523848",
+  appId: "1:961839523848:web:d3eb2a9ffb818ea92989bd",
+  measurementId: "G-L7L88FL39G"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = firebase.firestore();
+
+
+document.getElementById('formulario').addEventListener('submit', (event) =>{
+    event.preventDefault()
+
+    //Validar campo nombre
+    let entradaNombre = document.getElementById('name')
+    let errorNombre = document.getElementById('nameError')
+
+    if(entradaNombre.value.trim() === ''){
+        errorNombre.textContent = '*Por favor, introducí tu nombre'
+        errorNombre.classList.add('error-message')
+    }else{
+        errorNombre.textContent = ''
+        errorNombre.classList.remove('error-message')
+    }
+
+    //Validar correo electrónico
+    let emailEntrada = document.getElementById('email')
+    let emailError = document.getElementById('emailError')
+    let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // Patrón de validación básico
+    if(!emailPattern.test(emailEntrada.value)){
+        emailError.textContent = '*Por favor, introducí un mail válido'
+        emailError.classList.add('error-message')
+    }else{
+        emailError.textContent = ''
+        emailError.classList.remove('error-message')
+    }
+
+    //Validar la contraseña
+    let contrasenaEntrada = document.getElementById('password')
+    let contrasenaError = document.getElementById('passwordError')
+    let contrasenaPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&#.$($)$-$_])[A-Za-z\d$@$!%*?&#.$($)$-$_]{8,15}$/;
+    if(!contrasenaPattern.test(contrasenaEntrada.value)){
+        contrasenaError.textContent = '*La contraseña debe tener al menos 8 caracteres, números, mayúsculas y minúsculas y caracteres especiales'
+        contrasenaError.classList.add('error-message')
+    }else{
+        contrasenaError.textContent = ''
+        contrasenaError.classList.remove('error-message')
+    }
+
+    //Si todos los campos son válidos enviar formulario
+
+    if(!errorNombre.textContent && !emailError.textContent && !contrasenaError.textContent){
+
+        //BACKEND QUE RECIBA LA INFORMACIÓN
+
+        db.collection("users").add({
+            nombre: entradaNombre.value,
+            email: emailEntrada.value,
+            password: contrasenaEntrada.value
+        })
+        .then((docRef) => {
+            alert('El formulario se ha enviado con éxito', docRef.id)
+            document.getElementById('formulario').reset();
+        })
+        .catch((error) => {
+            alert(error)
+        });
+
+        
+    }
+
+})
